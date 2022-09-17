e4812f70171d5046e3c99eab2e07ecab99c940de
alphakahere <alphakahere@gmail.com>
2022-09-17 17:34:05 +0000

    configuration des routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Accueil from "./components/Accueil";
+import Apropos from "./components/Apropos";
 import Navbar from "./components/Navbar";
+import Tutoriels from "./components/Tutoriels";
 
 function App() {
 	return (
 		<div className="app">
 			<Navbar />
+			<Routes>
+				<Route path="/" element={<Accueil />} />
+				<Route path="tutoriels" element={<Tutoriels />} />
+				<Route path="a-propos" element={<Apropos />} />
+			</Routes>
 		</div>
 	);
 }
