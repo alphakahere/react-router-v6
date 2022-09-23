9e17b751c57292439e2afbc65ce3a1f35cf47f43
alphakahere <alphakahere@gmail.com>
2022-09-23 21:51:18 +0000

    non correspondance de route

---
+++ src/components/Page404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page404 = () => {
+	return (
+		<div className="pt-5 text-center">
+			<h1>Oops!</h1>
+			<p>Aucune page a été trouvée.</p>
+		</div>
+	);
+};
+
+export default Page404;
