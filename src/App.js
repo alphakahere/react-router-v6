5fd7fb191b02ce104a0778125b48864b61fce01d
alphakahere <alphakahere@gmail.com>
2022-10-01 11:29:33 +0000

    --amend

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Accueil from "./components/Accueil";
 import Apropos from "./components/Apropos";
 import Contact from "./components/Contact";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Page404 from "./components/Page404";
 import Tutoriels from "./components/Tutoriels";
 
 function App() {
 	return (
 		<div className="app">
-			{/* <Navbar /> */}
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Accueil />} />
 				<Route path="tutoriels" element={<Tutoriels />} />
