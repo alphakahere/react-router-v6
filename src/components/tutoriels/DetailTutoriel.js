a1d4d51484afd140c6819d5da439d040f432a0cb
alphakahere <alphakahere@gmail.com>
2022-10-13 07:22:19 +0000

    Search params

--- src/components/tutoriels/DetailTutoriel.js
+++ src/components/tutoriels/DetailTutoriel.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const DetailTutoriel = () => {
+	const params = useParams();
+	// const { tutorielId } = useParams();
+	// console.log({ tutorielId });
 	return (
 		<div>
-			<h1>Detail Tutoriel</h1>
+			<h1>Detail Tutoriel {params?.tutorielId}</h1>
 		</div>
 	);
 };
--- src/components/tutoriels/Tutoriels.js
+++ src/components/tutoriels/Tutoriels.js
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const Tutoriels = () => {
 	return (
 		<div className="px-4">
-			<h1>Mes Tutoriels</h1>
 			<div>
 				<Outlet />
 			</div>
