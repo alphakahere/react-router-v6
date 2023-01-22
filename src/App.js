bcd877adf047555d17cae494a9af3d33ee2f2e52
alphakahere <alphakahere@gmail.com>
2023-01-22 23:38:31 +0000

    utilisation de create browser router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import Accueil from "./components/Accueil";
 import Apropos from "./components/Apropos";
 import Contact from "./components/Contact";
@@ -9,23 +9,51 @@ import AjouterUnTutoriel from "./components/tutoriels/AjouterUnTutoriel";
 import DetailTutoriel from "./components/tutoriels/DetailTutoriel";
 import ListesDesTutoriels from "./components/tutoriels/ListesDesTutoriels";
 
-function App() {
+function AppLayout() {
 	return (
 		<div className="app">
 			<Navbar />
-			<Routes>
-				<Route path="/" element={<Accueil />} />
-				<Route path="tutoriels" element={<Tutoriels />}>
-					<Route index element={<ListesDesTutoriels />} />
-					<Route path=":tutorielId" element={<DetailTutoriel />} />
-					<Route path="ajouter" element={<AjouterUnTutoriel />} />
-				</Route>
-				<Route path="a-propos" element={<Apropos />} />
-				<Route path="contactez-nous" element={<Contact />} />
-				<Route path="*" element={<Page404 />} />
-			</Routes>
+			<Outlet />
 		</div>
 	);
 }
 
-export default App;
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <AppLayout />,
+		children: [
+			{
+				element: <Accueil />,
+				index: true,
+			},
+			{
+				path: "tutoriels",
+				element: <Tutoriels />,
+				children: [
+					{
+						element: <ListesDesTutoriels />,
+						index: true,
+					},
+					{
+						path: ":tutorielId",
+						element: <DetailTutoriel />,
+					},
+					{
+						path: "ajouter",
+						element: <AjouterUnTutoriel />,
+					},
+				],
+			},
+			{
+				path: "contactez-nous",
+				element: <Contact />,
+			},
+			{
+				path: "a-propos",
+				element: <Apropos />,
+			},
+		],
+		errorElement: <Page404 />,
+	},
+]);
